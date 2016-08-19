f66db18a33c7a4f041921c6ebea07792572de87d
loveKino <842914439@qq.com>
2016-08-19 13:35:09 +0800

    opt

--- src/first.js
+++ src/first.js
@@ -4,16 +4,30 @@ let syncPageState = require('./syncPageState');
 
 let isFirstInWindow = require('./isFirstInWindow');
 
+let {
+    startMomenter
+} = require('doming');
+
+let startMoment = startMomenter();
+
 let first = (memory, winId, entrance) => {
-    return isFirstInWindow(memory, winId).then(res => {
-        if (res) {
-            // sync page state
-            syncPageState(entrance);
-            // hack. js wont end here.
-            window.location.reload();
-            return true;
+    return startMoment().then(() => {
+        // hack fail to load page in chrome
+        if (window.location.href === 'data:text/html,chromewebdata') {
+            throw new Error('fail to load page!');
         } else {
-            return false;
+            return isFirstInWindow(memory, winId).then(res => {
+                if (res) {
+                    // sync page state
+                    syncPageState(entrance);
+                    // hack. js wont end here.
+                    window.location.reload();
+                    return true;
+                } else {
+                    return false;
+                }
+            });
+
         }
     });
 };
